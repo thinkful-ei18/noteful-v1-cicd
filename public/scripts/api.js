c82c6df3716d54cbec13e0b10df2770a1ff1e275
Chris Klanac <cklanac@users.noreply.github.com>
2018-01-27 12:43:23 -0600

    feat: simplify api.js

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -1,9 +1,8 @@
 /* global $ */
 'use strict';
 
-const api = (function () {
-
-  const search = function(query, callback) {
+const api = {
+  search: function (query, callback) {
     $.ajax({
       type: 'GET',
       url: '/v1/notes/',
@@ -11,20 +10,13 @@ const api = (function () {
       data: query,
       success: callback
     });
-  };
-
-  const details = function(id, callback) {
+  },
+  details: function (id, callback) {
     $.ajax({
       type: 'GET',
       dataType: 'json',
       url: `/v1/notes/${id}`,
       success: callback
     });
-  };
-
-  return {
-    search,
-    details
-  };
-  
-}());
+  }
+}; 
